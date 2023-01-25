8d911a235363f60261f10e1048b7d913f4c06867
Daniel Rubin <daniel.rubin1997@gmail.com>
2023-01-25 14:16:30 +0100

    Worked on views

--- board/models.py
+++ board/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Todo(models.Model):
+    created_at = models.DateField(default=date.today)
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    deadline = models.DateField()
--- scrum_board/urls.py
+++ scrum_board/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from board.views import board
+from board.views import board, register_request, login_request, logout_request
 
 urlpatterns = [
+    path("", login_request, name="login"),
+    path("login/", login_request, name="login"),
     path('admin/', admin.site.urls),
     path('board/', board),
+    path("register/", register_request, name="register"),
+    path("logout/", logout_request, name="logout"),
+
 ]
